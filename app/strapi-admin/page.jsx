576555bd900574c036bf85b3f61fa15aafb844a0
Maria Carolina Moreno <63366140+mariacairam@users.noreply.github.com>
2024-04-03 15:25:28 -0300

    page

--- app/strapi-admin/page.jsx
+++ app/strapi-admin/page.jsx
@@ -1,12 +1,10 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-
-function MyComponent() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('https://teste-cms-strapi-2-production.up.railway.app/admin');
-  }, []);
-
-  return <div>Redirecting...</div>;
+import { useRouter } from 'next/router'
+ 
+export default function Page() {
+    const router = useRouter();
+    useEffect(() => {
+        router.replace('https://teste-cms-strapi-2-production.up.railway.app/admin');
+      }, []);
+    
+      return (<div>Redirecting...</div>);
 }
