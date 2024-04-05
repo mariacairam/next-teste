6a4378d70e5d7bdacdf9d0a7a680d58beffd00f5
Maria Carolina Moreno <63366140+mariacairam@users.noreply.github.com>
2024-04-05 16:19:55 -0300

    footer e contact

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,15 +1,18 @@
+const nodemailer = require('nodemailer');
+
 export async function POST(request) {
   try {
     const contact = await request.json();
     console.log(contact);
 
-    const nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      port: 465,
+      host: "smtp.gmail.com",
       auth: {
         user: process.env.GMAIL_EMAIL_ADDRESS,
         pass: process.env.GMAIL_APP_PASSWORD
-      }
+      },
+      secure: true,
     });
 
     const message = {
@@ -20,12 +23,31 @@ export async function POST(request) {
       html: `<p>${contact.message}</p>`,
     };
 
-    transporter.sendMail(message, function (err, info) {
-      if(err)
-        console.log(err)
-      else
-        console.log(info)
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error, success) {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              console.log("Server is ready to take our messages");
+              resolve(success);
+          }
+      });
     });
+
+    await new Promise ((resolve, reject) => {
+      transporter.sendMail(message, function (err, info) {
+        if(err){
+          console.log(err);
+          reject(err);
+        }
+        else{
+          console.log(info);
+          resolve(info);
+        }
+      });
+    });
+    
 
     return new Response('Received JSON data successfully', {
       status: 200,
